67082d765349fea9792a76737a1cc0531ca7ea57
ajimura <ajimuralk@gmail.com>
2019-03-05 22:34:27 -0800

    Finish WarehouseList tablet view

--- client/src/components/warehouse-components/Warehouse.jsx
+++ client/src/components/warehouse-components/Warehouse.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import arrow from '../../assets/logos/Icon-arrow-right.svg';
 
 class Warehouse extends Component {
   render() {
@@ -6,9 +7,12 @@ class Warehouse extends Component {
     return (
       <div>
         <tr className="table__row">
-          <div>
+          <div className="warehouseMainInfo">
             <td className="warehouseName">{data.warehouseName}</td>
-            <td>{data.address}</td>
+            <td>
+              {data.address}
+              <img src={arrow} className="arrow" alt="link" />
+            </td>
           </div>
           <div>
             <td>{data.contact}</td>
--- client/src/components/warehouse-components/WarehouseList.jsx
+++ client/src/components/warehouse-components/WarehouseList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Warehouse from './Warehouse';
-import './styles/warehouse.css'
+import './styles/warehouse.css';
 
 const MOCK_DATA = [
   {
@@ -43,21 +43,20 @@ const MOCK_DATA = [
 
 class WarehouseList extends Component {
   render() {
-
     const warehouseList = MOCK_DATA.map((warehouse, i) => {
       return <Warehouse {...warehouse} key={i} />;
     });
 
     return (
-      <div className='WarehouseList'>
-        <div className='WarehouseList__header'>
-          <h1 className='header__h1'>Locations</h1>
-          <input className='header__search' type='text' placeholder='Search' />
+      <div className="WarehouseList">
+        <div className="WarehouseList__header">
+          <h1 className="header__h1">Locations</h1>
+          <input className="header__search" type="text" placeholder="Search" />
         </div>
 
-        <table className='WarehouseList__table'>
+        <table className="WarehouseList__table">
           <thead>
-            <tr className='table__header'>
+            <tr className="table__header">
               <th>Warehouse</th>
               <th>Contact</th>
               <th>Contact Information</th>
