7ed3f3cbb6f901313fdf67d50e1044a17db345a8
Tasneem Kurtu <tasneemkurtu@gmail.com>
2019-03-05 19:21:41 -0800

    Navbar.js rename

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import './components/Navbar/Navbar';
+// import Navbar from './components/Navbar/Navbar';
 import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
