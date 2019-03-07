32af826de1fb05127932346c1158ec6c9f8d4658
moth-man <mothmanor@gmail.com>
2019-03-07 12:05:45 -0800

    Create local routes in app.

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import WarehouseList from './components/warehouse-components/WarehouseList';
 import InventoryList from './components/inventory-components/InventoryList';
+import { Switch, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <InventoryList inventory={this.props} />
-        <WarehouseList />
+        <Switch>
+          <Route path="/" exact component={WarehouseList} />
+          <Route path="/inventory" render={(routeProps) => (<InventoryList {...routeProps}
+            inventory={this.props}
+          />)}
+          />
+        </Switch>
+        {/* <InventoryList inventory={this.props} /> */}
       </div>
     );
   }
