34ea8d152293914727d3adec0fa275b555a388fd
ajimura <ajimuralk@gmail.com>
2019-03-06 23:02:52 -0800

    Created initial post end point

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
 const DATA = require("../data/warehouse.json");
 
-router.route("/").get((req, res) => {
-    res.json(DATA);
-});
+router
+    .route("/")
+    .get((req, res) => {
+        res.json(DATA);
+    })
+    .post((req, res) => {
+        const {
+            warehouseName,
+            address,
+            contact,
+            contactTitle,
+            contactNum,
+            contactEmail,
+            categories
+        } = req.body;
+
+        const newWarehouse = {
+            warehouseName,
+            address,
+            contact,
+            contactTitle,
+            contactNum,
+            contactEmail,
+            categories
+        };
+        res.send(newWarehouse);
+    });
 
 module.exports = router;
