6a2ac5d713c46bee0b69832ede71cf880ab0ef8e
ajimura <ajimuralk@gmail.com>
2019-03-06 20:24:18 -0800

    Populate warehouse JSON file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const PORT = process.argv[2] || process.env || 8080;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const PORT = process.argv[2] || process.env.PORT || 8080;
+const warehouseRoutes = require("./routes/warehouse");
 
-app.use(bodyParser.json())
 
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/warehouses", warehouseRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Listening on Port ${PORT}...`)
-})
+    console.log(`Listening on Port ${PORT}...`);
+});
 
 //installed : cors, body-parser, express-router, express
