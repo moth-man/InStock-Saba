2350ac39e45d8cd4c5e5a81a8dfad50d95885fcc
rbanh <rbanh@me.com>
2019-03-06 22:39:31 -0800

    got the overlay to dissolve

--- client/src/components/CreateNewModal/CreateNew.jsx
+++ client/src/components/CreateNewModal/CreateNew.jsx
@@ -51,96 +51,100 @@ class CreateNew extends Component {
     });
     onClick.preventDefault();
     this.refs.modal.style.display = "none";
+    this.refs.overlay.style.display = "none";
   };
 
   cancel = onClick => {
     onClick.preventDefault();
     this.refs.modal.style.display = "none";
+    this.refs.overlay.style.display = "none";
   };
 
   render() {
     return (
-      <div className='modal' ref='modal'>
-        <form className='modal-content'>
-          <h3>Create New</h3>
-          <div className='flex-wrap'>
-            <div className='flex-column'>
-              <h5>PRODUCT</h5>
-              <input
-                type='text'
-                placeholder='Item Name'
-                name='item_name'
-                onChange={this.update_input_state}
-              />
-            </div>
-            <div className='flex-column'>
-              <h5>LAST ORDERED</h5>
-              <input
-                type='text'
-                placeholder='yyyy-mm-dd'
-                name='date'
-                onChange={this.update_input_state}
-              />
-            </div>
-            <div className='flex-column'>
-              <h5>CITY</h5>
-              <input
-                type='text'
-                placeholder='City'
-                name='city'
-                onChange={this.update_input_state}
-              />
-            </div>
-            <div className='flex-column'>
-              <h5>COUNTRY</h5>
-              <select name='country' onChange={this.update_input_state}>
-                <option value='' />
-                <option value='Canada'>Canada</option>
-              </select>
-            </div>
-            <div className='flex-column'>
-              <h5>QUANTITY</h5>
-              <input
-                type='text'
-                name='quantity_items'
-                id=''
-                placeholder='0'
-                onChange={this.update_input_state}
-              />
-            </div>
-            <div className=''>
+      <div className='overlay' ref='overlay'>
+        <div className='modal' ref='modal'>
+          <form className='modal-content'>
+            <h3>Create New</h3>
+            <div className='flex-wrap'>
               <div className='flex-column'>
-                <h5>STATUS</h5>
-                <div className='flex-row spacebetween'>
-                  <p>In Stock</p>
-                  <Switch
-                    onChange={this.handleChange}
-                    checked={this.state.instock}
-                    className='react-switch'
-                    uncheckedIcon={false}
-                    checkedIcon={false}
-                    onColor='#6aae2b'
-                  />
+                <h5>PRODUCT</h5>
+                <input
+                  type='text'
+                  placeholder='Item Name'
+                  name='item_name'
+                  onChange={this.update_input_state}
+                />
+              </div>
+              <div className='flex-column'>
+                <h5>LAST ORDERED</h5>
+                <input
+                  type='text'
+                  placeholder='yyyy-mm-dd'
+                  name='date'
+                  onChange={this.update_input_state}
+                />
+              </div>
+              <div className='flex-column'>
+                <h5>CITY</h5>
+                <input
+                  type='text'
+                  placeholder='City'
+                  name='city'
+                  onChange={this.update_input_state}
+                />
+              </div>
+              <div className='flex-column'>
+                <h5>COUNTRY</h5>
+                <select name='country' onChange={this.update_input_state}>
+                  <option value='' />
+                  <option value='Canada'>Canada</option>
+                </select>
+              </div>
+              <div className='flex-column'>
+                <h5>QUANTITY</h5>
+                <input
+                  type='text'
+                  name='quantity_items'
+                  id=''
+                  placeholder='0'
+                  onChange={this.update_input_state}
+                />
+              </div>
+              <div className=''>
+                <div className='flex-column'>
+                  <h5>STATUS</h5>
+                  <div className='flex-row spacebetween'>
+                    <p>In Stock</p>
+                    <Switch
+                      onChange={this.handleChange}
+                      checked={this.state.instock}
+                      className='react-switch'
+                      uncheckedIcon={false}
+                      checkedIcon={false}
+                      onColor='#6aae2b'
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <h5>ITEM DESCRIPTION</h5>
-          <textarea
-            name='item_description'
-            id=''
-            placeholder='(Optional)'
-            onChange={this.update_input_state}
-          />
-          <div className='flex-column movedatbutton'>
-            <div className='flex-column button'>
-              <button onClick={this.post}>SAVE</button>
-              <button onClick={this.cancel} className='cancel'>
-                CANCEL
-              </button>
+            <h5>ITEM DESCRIPTION</h5>
+            <textarea
+              name='item_description'
+              id=''
+              placeholder='(Optional)'
+              onChange={this.update_input_state}
+            />
+            <div className='flex-column movedatbutton'>
+              <div className='flex-column button'>
+                <button onClick={this.post}>SAVE</button>
+                <button onClick={this.cancel} className='cancel'>
+                  CANCEL
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     );
   }
