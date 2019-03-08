47bab962888327134f17b88af7e6f6679d55ca83
moth-man <mothmanor@gmail.com>
2019-03-08 15:39:58 -0800

    Add currentwarehouse component to warehouse link.

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,64 @@ import Navbar from './components/Navbar/Navbar';
 import WarehouseList from './components/warehouse-components/WarehouseList';
 import InventoryList from './components/inventory-components/InventoryList';
 import { Switch, Route, Link } from 'react-router-dom'
+import axios from 'axios'
+
+const warehouseURL = `http://localhost:8080/warehouses`;
+const inventoryURL = `http://localhost:8080/inventory`;
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      warehouses: [],
+      inventory: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(warehouseURL)
+      .then(({ data }) => {
+        this.setState({
+          warehouses: data
+        })
+      })
+      .then(() => {
+        axios
+          .get(inventoryURL)
+          .then(({ data }) => {
+            this.setState({
+              inventory: data
+            })
+          })
+      })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={WarehouseList} />
-          <Route path="/inventory" render={(routeProps) => (<InventoryList {...routeProps}
-
-          />)}
-          />
-          <Route path="/warehouse/:id" render={(routeProps) => (<InventoryList {...routeProps}
-
-            id={this.props.match}
-          />)}
-          />
-        </Switch>
-        {/* <InventoryList inventory={this.props} /> */}
-      </div>
-    );
+    if (this.state.inventory.length === 0 || !this.state.warehouses.length === 0) {
+      console.log("@", this.state)
+      return <h1>Loading</h1>
+    } else {
+      console.log("!", this.state)
+      return (
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={WarehouseList} />
+            <Route path="/inventory" render={(routeProps) => (<InventoryList {...routeProps}
+              inventory={this.state.inventory}
+              warehouses={this.state.warehouses}
+            />)}
+            />
+            <Route path="/warehouse/:id" render={(routeProps) => (<InventoryList {...routeProps}
+              inventory={this.state.inventory}
+              warehouses={this.state.warehouses}
+            />)}
+            />
+          </Switch>
+        </div>
+      );
+    }
   }
 }
 
