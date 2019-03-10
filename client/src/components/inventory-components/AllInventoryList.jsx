bb427ff2085a7ab0f01a8c1edd6b8b380b688378
rbanh <rbanh@me.com>
2019-03-09 18:07:30 -0800

    added the modals to all pages and made funct

--- client/src/components/inventory-components/AllInventoryList.jsx
+++ client/src/components/inventory-components/AllInventoryList.jsx
@@ -1,80 +1,87 @@
-import React, { Component } from 'react'
-import InventoryItem from './InventoryItem'
-import './Inventory.css'
+import React, { Component } from "react";
+import InventoryItem from "./InventoryItem";
+import "./Inventory.css";
+import CreateNewInvModal from "./CreateNewInvModal";
 
 class InventoryList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      inventoryList: []
-    }
+      inventoryList: [],
+      showModal: false
+    };
   }
 
-  iList = (inventory) => {
+  iList = inventory => {
     if (inventory) {
-      let list = []
+      let list = [];
       inventory.map(i => {
         i.slice(1).map(iv => {
-          list.push(iv)
-        })
-      })
+          list.push(iv);
+        });
+      });
       let iList = list.map(item => {
         if (item.status === true) {
-          item.status = "In-Stock"
+          item.status = "In-Stock";
         } else {
-          item.status = "Not In-Stock"
+          item.status = "Not In-Stock";
         }
-        return (
-          <InventoryItem {...item} />
-        )
-      })
+        return <InventoryItem {...item} />;
+      });
       this.setState({
         inventoryList: iList
-      })
-      console.log(list)
+      });
+      console.log(list);
     }
-
-  }
-
-
+  };
 
   addInventory = () => {
-    console.log("clicked")
-  }
+    console.log("clicked");
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  };
 
   componentDidMount() {
-    const { inventory } = this.props
-    this.iList(inventory)
+    const { inventory } = this.props;
+    this.iList(inventory);
   }
 
   render() {
-
     return (
-
-      <div className="inventoryList__container">
-        <h1 className="inventoryList__title">Inventory</h1>
-        <div className="searchBar__container">
-          <form className="searchBar">
-            <input className="searchBar__input" type="text" placeholder="Search"></input>
+      <div className='inventoryList__container'>
+        <h1 className='inventoryList__title'>Inventory</h1>
+        <div className='searchBar__container'>
+          <form className='searchBar'>
+            <input
+              className='searchBar__input'
+              type='text'
+              placeholder='Search'
+            />
           </form>
         </div>
-        <table className="inventoryList__table">
-          <thead className="table__head">
-            <tr className="table__head__row">
-              <th className="inventoryList__th item">ITEM</th>
-              <th className="inventoryList__th">LAST ORDERED</th>
-              <th className="inventoryList__th">LOCATION</th>
-              <th className="inventoryList__th">QUANTITY</th>
-              <th className="inventoryList__th">STATUS</th>
+        {this.state.showModal && <CreateNewInvModal />}
+        <table className='inventoryList__table'>
+          <thead className='table__head'>
+            <tr className='table__head__row'>
+              <th className='inventoryList__th item'>ITEM</th>
+              <th className='inventoryList__th'>LAST ORDERED</th>
+              <th className='inventoryList__th'>LOCATION</th>
+              <th className='inventoryList__th'>QUANTITY</th>
+              <th className='inventoryList__th'>STATUS</th>
             </tr>
           </thead>
           <tbody>{this.state.inventoryList}</tbody>
         </table>
-        <button className="add__inventoryItem__button" onClick={() => this.addInventory()}><div className="add__inventoryItem__button__container"></div></button>
+        <button
+          className='add__inventoryItem__button'
+          onClick={() => this.addInventory()}
+        >
+          <div className='add__inventoryItem__button__container' />
+        </button>
       </div>
-    )
+    );
   }
 }
 
-
-export default InventoryList
+export default InventoryList;
---
+++ client/src/components/inventory-components/CreateNewInvModal.jsx
@@ -0,0 +1,160 @@
+import React, { Component } from "react";
+import "./CreateNewModal.css";
+import Switch from "react-switch";
+import axios from "axios";
+const url = "blah.ca";
+
+class CreateNewInvModal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      instock: false,
+      item_name: "",
+      date: "",
+      city: "",
+      country: "",
+      quantity_items: "",
+      item_description: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  post = onClick => {
+    if (
+      !this.state.item_name ||
+      !this.state.date ||
+      !this.state.city ||
+      !this.state.country ||
+      !this.state.quantity_items
+    ) {
+      console.log(
+        this.state.item_name,
+        this.state.date,
+        this.state.city,
+        this.state.country,
+        this.state.quantity_items
+      );
+      onClick.preventDefault();
+      return alert("Please enter all required input fields");
+    }
+    axios.post(url, {
+      instock: this.state.instock,
+      item_name: this.state.item_name,
+      date: this.state.date,
+      city: this.state.city,
+      country: this.state.country,
+      quantity_items: this.state.quantity_items,
+      item_description: this.state.item_description
+    });
+    onClick.preventDefault();
+    this.refs.modal.style.display = "none";
+    this.refs.overlay.style.display = "none";
+  };
+
+  cancel = onClick => {
+    onClick.preventDefault();
+    this.refs.modal.style.display = "none";
+    this.refs.overlay.style.display = "none";
+  };
+
+  handleChange(instock) {
+    this.setState({ instock });
+  }
+
+  update_input_state = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className='overlay' ref='overlay'>
+        <div className='modal' ref='modal'>
+          <form className='modal-content'>
+            <h3>Create New</h3>
+            <div className='flex-wrap'>
+              <div className='flex-column'>
+                <h5>PRODUCT</h5>
+                <input
+                  type='text'
+                  placeholder='Item Name'
+                  name='item_name'
+                  onChange={this.update_input_state}
+                />
+              </div>
+              <div className='flex-column'>
+                <h5>LAST ORDERED</h5>
+                <input
+                  type='text'
+                  placeholder='yyyy-mm-dd'
+                  name='date'
+                  onChange={this.update_input_state}
+                />
+              </div>
+              <div className='flex-column'>
+                <h5>CITY</h5>
+                <input
+                  type='text'
+                  placeholder='City'
+                  name='city'
+                  onChange={this.update_input_state}
+                />
+              </div>
+              <div className='flex-column'>
+                <h5>COUNTRY</h5>
+                <select name='country' onChange={this.update_input_state}>
+                  <option value='' />
+                  <option value='Canada'>Canada</option>
+                </select>
+              </div>
+              <div className='flex-column'>
+                <h5>QUANTITY</h5>
+                <input
+                  type='text'
+                  name='quantity_items'
+                  id=''
+                  placeholder='0'
+                  onChange={this.update_input_state}
+                />
+              </div>
+              <div className=''>
+                <div className='flex-column'>
+                  <h5>STATUS</h5>
+                  <div className='flex-row spacebetween'>
+                    <p>In Stock</p>
+                    <Switch
+                      onChange={this.handleChange}
+                      checked={this.state.instock}
+                      className='react-switch'
+                      uncheckedIcon={false}
+                      checkedIcon={false}
+                      onColor='#6aae2b'
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <h5>ITEM DESCRIPTION</h5>
+            <textarea
+              name='item_description'
+              id=''
+              placeholder='(Optional)'
+              onChange={this.update_input_state}
+            />
+            <div className='flex-column movedatbutton'>
+              <div className='flex-column button'>
+                <button onClick={this.post}>SAVE</button>
+                <button onClick={this.cancel} className='cancel'>
+                  CANCEL
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreateNewInvModal;
