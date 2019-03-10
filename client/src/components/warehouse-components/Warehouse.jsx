047e0ffb01b96732ade2eb718809bb2cf7b46f05
rbanh <rbanh@me.com>
2019-03-09 20:46:36 -0800

    blah

--- client/src/components/warehouse-components/Warehouse.jsx
+++ client/src/components/warehouse-components/Warehouse.jsx
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import arrow from '../../assets/logos/Icon-arrow-right.svg';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import arrow from "../../assets/logos/Icon-arrow-right.svg";
 
 class Warehouse extends Component {
   render() {
     const { ...data } = this.props;
-    const link = `/warehouse/${data.id}`
+    const link = `/warehouse/${data.id}`;
     return (
       <div>
-        <tr className="table__row">
-          <div className="warehouseMainInfo">
-            <td className="warehouseName">{data.warehouseName}</td>
+        <tr className='table__row'>
+          <div className='warehouseMainInfo'>
+            <td className='warehouseName'>{data.warehouseName}</td>
             <td>
               {data.address}
               <Link to={link}>
-                <img src={arrow} className="arrow-mobile" alt="link" />
+                <img src={arrow} className='arrow-mobile' alt='link' />
               </Link>
             </td>
           </div>
           <div>
             <td>{data.contact}</td>
-            <td className="contact">{data.contactTitle}</td>
+            <td className='contact'>{data.contactTitle}</td>
           </div>
           <div>
             <td>{data.contactNum}</td>
             <td>{data.contactEmail}</td>
           </div>
-          <div className="categoryInfo">
-            <td className="category__td">
+          <div className='categoryInfo'>
+            <td className='category__td'>
               {data.categories}
-              <img src={arrow} className="arrow-desktop" alt="link" />
+              <img src={arrow} className='arrow-desktop' alt='link' />
             </td>
           </div>
         </tr>
