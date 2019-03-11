b3db0f94366dd64bc8a16a89faacc15ce8471acb
rbanh <rbanh@me.com>
2019-03-11 00:46:07 -0700

    fixed a bunch of stuff

--- client/src/components/WarehouseModal1/warehouseModal.jsx
+++ client/src/components/WarehouseModal1/warehouseModal.jsx
@@ -45,92 +45,88 @@ class Warehouse extends Component {
     });
     onClick.preventDefault();
     this.props.formSubmit();
-    this.refs.modal.style.display = "none";
     this.refs.overlay.style.display = "none";
   };
 
   cancel = onClick => {
     onClick.preventDefault();
-    this.refs.modal.style.display = "none";
     this.refs.overlay.style.display = "none";
   };
 
   render() {
     return (
       <div className='overlay' ref='overlay'>
-        <div className='modal' ref='modal'>
-          <form className='modal-content'>
-            <h3>Create New</h3>
-            <div className='flex-wrap'>
-              <div className='flex-column'>
-                <h5>WAREHOUSE NAME</h5>
-                <input
-                  type='text'
-                  placeholder='Warehouse Name'
-                  name='warehouseName'
-                  onChange={this.update_input_state}
-                />
-              </div>
+        <form className='modal-content'>
+          <h3>Create New</h3>
+          <div className='flex-wrap'>
+            <div className='flex-column'>
+              <h5>WAREHOUSE NAME</h5>
+              <input
+                type='text'
+                placeholder='Warehouse Name'
+                name='warehouseName'
+                onChange={this.update_input_state}
+              />
+            </div>
 
+            <div className='flex-column'>
+              <h5>ADDRESS</h5>
+              <input
+                type='text'
+                placeholder='123 1st Ave, Vancouver, BC'
+                name='address'
+                onChange={this.update_input_state}
+              />
+            </div>
+            <div className='flex-column'>
               <div className='flex-column'>
-                <h5>ADDRESS</h5>
+                <h5>CONTACT NAME</h5>
                 <input
                   type='text'
-                  placeholder='123 1st Ave, Vancouver, BC'
-                  name='address'
-                  onChange={this.update_input_state}
-                />
-              </div>
-              <div className='flex-column'>
-                <div className='flex-column'>
-                  <h5>CONTACT NAME</h5>
-                  <input
-                    type='text'
-                    placeholder='Joan Smith'
-                    name='contact'
-                    onChange={this.update_input_state}
-                  />
-                </div>
-              </div>
-              <div className='flex-column'>
-                <h5>CONTACT NUMBER</h5>
-                <input
-                  type='text'
-                  placeholder='123-232-2222'
-                  name='contactNum'
-                  onChange={this.update_input_state}
-                />
-              </div>
-              <div className='flex-column'>
-                <h5>EMAIL</h5>
-                <input
-                  type='text'
-                  name='contactEmail'
-                  id=''
-                  placeholder='123@instock.com'
-                  onChange={this.update_input_state}
-                />
-              </div>
-              <div className='flex-column'>
-                <h5>CATEGORIES</h5>
-                <input
-                  type='text'
-                  placeholder='Industrial, Electronics'
-                  name='categories'
+                  placeholder='Joan Smith'
+                  name='contact'
                   onChange={this.update_input_state}
                 />
               </div>
             </div>
-            <div className='flex-column movedatbutton'>
-              <div className='flex-column button'>
-                <button onClick={this.post}>SAVE</button>
-                <button onClick={this.cancel} className='cancel'>
-                  CANCEL
-                </button>
-              </div>
+            <div className='flex-column'>
+              <h5>CONTACT NUMBER</h5>
+              <input
+                type='text'
+                placeholder='123-232-2222'
+                name='contactNum'
+                onChange={this.update_input_state}
+              />
             </div>
-          </form>
-        </div>
+            <div className='flex-column'>
+              <h5>EMAIL</h5>
+              <input
+                type='text'
+                name='contactEmail'
+                id=''
+                placeholder='123@instock.com'
+                onChange={this.update_input_state}
+              />
+            </div>
+            <div className='flex-column'>
+              <h5>CATEGORIES</h5>
+              <input
+                type='text'
+                placeholder='Industrial, Electronics'
+                name='categories'
+                onChange={this.update_input_state}
+              />
+            </div>
+          </div>
+          <div className='flex-column movedatbutton'>
+            <div className='flex-column button'>
+              <button onClick={this.post}>SAVE</button>
+              <button onClick={this.cancel} className='cancel'>
+                CANCEL
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
