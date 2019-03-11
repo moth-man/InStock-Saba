36a71c29bd5d8d1fe5d709232439f328c67e5af3
rbanh <rbanh@me.com>
2019-03-10 20:39:18 -0700

    finished formating for warehouse with its products

--- client/src/components/warehouse-components/CurrentWarehouse.jsx
+++ client/src/components/warehouse-components/CurrentWarehouse.jsx
@@ -1,35 +1,39 @@
-import React, { Component } from 'react';
-import arrow from '../../assets/logos/Icon-back-arrow.svg';
-import './warehouse-styles/currentWarehouse.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import arrow from "../../assets/logos/Icon-back-arrow.svg";
+import "./warehouse-styles/currentWarehouse.css";
+import { Link } from "react-router-dom";
 
 class CurrentWarehouse extends Component {
   render() {
     const { ...data } = this.props;
     return (
-      <div className="currentWarehouse__container">
-        <tr className="current__table__row">
-          <div className="currentWarehouseMainInfo">
-            <div className="currentWarehouse__title__container">
-              <Link to={'/'}>
-                <img src={arrow} className="current__arrow-mobile" alt="link" />
+      <div className='currentWarehouse__container'>
+        <tr className='current__table__row'>
+          <div className='currentWarehouseMainInfo'>
+            <div className='currentWarehouse__title__container'>
+              <Link to={"/"}>
+                <img src={arrow} className='current__arrow-mobile' alt='link' />
               </Link>
-              <h1 className="currentWarehouseName">{data.warehouseName}</h1>
+              <h1 className='currentWarehouseName'>{data.warehouseName}</h1>
             </div>
-            <div className="divider" />
-            <p className="currentWarehouse__address__title">ADDRESS</p>
-            <td>{data.address}</td>
           </div>
-          <div>
-            <p className="currentWarehouse__contact__title">CONTACT</p>
-            <td>{data.contact}</td>
-            <td className="contact">{data.contactTitle}</td>
+          <div className='flex'>
+            <div className='for-flex'>
+              <p className='currentWarehouse__address__title'>ADDRESS</p>
+              <div>{data.address}</div>
+            </div>
+            <div className='for-flex'>
+              <div>
+                <p className='currentWarehouse__contact__title'>CONTACT</p>
+                <div>{data.contact}</div>
+                <div className='contact'>{data.contactTitle}</div>
+              </div>
+              <div className='notgrey'>
+                <div>{data.contactNum}</div>
+                <div>{data.contactEmail}</div>
+              </div>
+            </div>
           </div>
-          <div>
-            <td>{data.contactNum}</td>
-            <td>{data.contactEmail}</td>
-          </div>
-          <div className="categoryInfo" />
         </tr>
       </div>
     );
