d073da760e484148880065693415b0364b576777
ajimura <ajimuralk@gmail.com>
2019-03-11 20:12:00 -0700

    Refactor warehouseModal to use refs rather than state

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar/navbar';
+import Navbar from './components/Navbar/Navbar';
 import WarehouseList from './components/warehouse-components/WarehouseList';
 import InventoryList from './components/inventory-components/InventoryList';
 import AllInventoryList from './components/inventory-components/AllInventoryList';
 import ProductModal from './components/productmodal-components/ProductModal';
-import { Switch, Route, Link } from 'react-router-dom'
-import axios from 'axios'
+import { Switch, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 
 const warehouseURL = `http://localhost:8080/warehouses`;
 const inventoryURL = `http://localhost:8080/inventory`;
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       warehouses: [],
       inventory: []
-    }
+    };
   }
 
   componentDidMount() {
@@ -25,37 +25,67 @@ class App extends Component {
       .then(({ data }) => {
         this.setState({
           warehouses: data
-        })
+        });
       })
       .then(() => {
-        axios
-          .get(inventoryURL)
-          .then(({ data }) => {
-            this.setState({
-              inventory: data
-            })
-          })
-      })
+        axios.get(inventoryURL).then(({ data }) => {
+          this.setState({
+            inventory: data
+          });
+        });
+      });
   }
 
+  formSubmit = (inputData) => {
+    axios
+      .post(warehouseURL, {
+        inputData
+      })
+      .then(({data}) => {
+        console.log(data)
+        this.setState({
+          warehouses: data
+        });
+      });
+  };
+
   render() {
-    if (this.state.inventory.length === 0 || !this.state.warehouses.length === 0) {
-      return <h1>Loading</h1>
+    if (
+      this.state.inventory.length === 0 ||
+      !this.state.warehouses.length === 0
+    ) {
+      return <h1>Loading</h1>;
     } else {
       return (
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path="/" exact component={WarehouseList} />
-            <Route path="/inventory/:id" component={ProductModal} />
-            <Route path="/inventory" render={(routeProps) => (<AllInventoryList {...routeProps}
-              inventory={this.state.inventory}
-            />)}
+            <Route
+              path="/"
+              exact
+              render={() => (
+                <WarehouseList formSubmit={() => this.formSubmit()} />
+              )}
             />
-            <Route path="/warehouse/:id" render={(routeProps) => (<InventoryList {...routeProps}
-              inventory={this.state.inventory}
-              warehouses={this.state.warehouses}
-            />)}
+            <Route path="/inventory/:id" component={ProductModal} />
+            <Route
+              path="/inventory"
+              render={routeProps => (
+                <AllInventoryList
+                  {...routeProps}
+                  inventory={this.state.inventory}
+                />
+              )}
+            />
+            <Route
+              path="/warehouse/:id"
+              render={routeProps => (
+                <InventoryList
+                  {...routeProps}
+                  inventory={this.state.inventory}
+                  warehouses={this.state.warehouses}
+                />
+              )}
             />
           </Switch>
         </div>
@@ -65,8 +95,6 @@ class App extends Component {
 }
 
 export default App;
-
-
 
 // import { Switch, Route, Link } from 'react-router-dom'
 
